b17c4e563cfd0b6c6f2d868f1c99ffd75e60b721
Joseph Ma <joseph@textql.com>
2025-08-24 15:05:02 -0400

    select all

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PurityQuestions from '../PurityQuestions';
 
 class QuestionList extends Component {
@@ -20,23 +20,62 @@ class QuestionList extends Component {
     }
   }
 
+  selectAll = () => {
+    const checkboxes = document.querySelectorAll('.form-check-input');
+    checkboxes.forEach((checkbox, index) => {
+      if (!checkbox.checked) {
+        checkbox.checked = true;
+        this.handleCheck({ target: checkbox }, PurityQuestions[index]);
+      }
+    });
+  }
+
+  deselectAll = () => {
+    const checkboxes = document.querySelectorAll('.form-check-input');
+    checkboxes.forEach((checkbox, index) => {
+      if (checkbox.checked) {
+        checkbox.checked = false;
+        this.handleCheck({ target: checkbox }, PurityQuestions[index]);
+      }
+    });
+  }
+
   render() {
     return (
-      PurityQuestions.map((question, index) => {
-        return (
-          <label key={question.id} className='list-group-item list-group-item-action d-flex align-items-center pl-2 shadow-sm'>
-            <span key={question.id} className='question-number'>{index + 1}</span>
-            <input 
-              key={question.id} 
-              className='form-check-input mr-3' type='checkbox' 
-              value={question.weight} onChange={ (e) => this.handleCheck(e, question) } 
-            />
-            {question.content}
-          </label>
-        );
-      })
+      <Fragment>
+        <div className='d-flex justify-content-between mb-3'>
+          <button 
+            type='button' 
+            className='btn btn-outline-teal'
+            onClick={this.selectAll}
+          >
+            Select All
+          </button>
+          <button 
+            type='button' 
+            className='btn btn-outline-teal'
+            onClick={this.deselectAll}
+          >
+            Deselect All
+          </button>
+        </div>
+        {PurityQuestions.map((question, index) => {
+          return (
+            <label key={question.id} className='list-group-item list-group-item-action d-flex align-items-center pl-2 shadow-sm'>
+              <span key={question.id} className='question-number'>{index + 1}</span>
+              <input 
+                key={question.id} 
+                className='form-check-input mr-3' 
+                type='checkbox' 
+                value={question.weight} 
+                onChange={ (e) => this.handleCheck(e, question) } 
+              />
+              {question.content}
+            </label>
+          );
+        })}
+      </Fragment>
     );
   }
-}
 
 export default QuestionList;
